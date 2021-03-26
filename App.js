be9a80af8c0b9b5717304e4727bd7fc37d113b63
jpitman1010 <jpitman1010@gmail.com>
2021-03-26 09:44:58 -0700

    changing nav bar styles

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Text, View } from 'react-native';
 import Home from './src/home';
 import Users from './src/users';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 
 
@@ -15,7 +16,29 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Users" component={Users} />
+        <Stack.Screen 
+        name="Users" 
+        options={{
+          title:"xUsers", 
+          headerStyle:{
+            backgroundColor:"red",
+            borderWidth: 5,
+            borderBottomColor: "black",
+            // color:"white", this doesn't work in header
+          
+          },
+           headerTintColor:"white",
+          //  you have to use this (read the docs https://reactnavigation.org/docs/headers) for more options
+          headerTitleStyle:{
+            fontWeight:'bold',
+            fontStyle: "italic",
+            textDecorationLine:'line-through'
+          }
+        }}
+        component={Users} 
+        initialParams={{id:1,codeName:"Pitboss"}}
+        //this is used to pass params that might not be there yet...
+        />
 
       </Stack.Navigator>
     </NavigationContainer>
