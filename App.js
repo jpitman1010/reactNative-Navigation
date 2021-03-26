55661c858cc2e18c9ab1a39664ed7a912bc8d4b9
jpitman1010 <jpitman1010@gmail.com>
2021-03-26 09:49:54 -0700

    making all headers have same css changes

--- App.js
+++ App.js
@@ -10,30 +10,39 @@ import { Header } from 'react-native/Libraries/NewAppScreen';
 
 
 const Stack = createStackNavigator();
+const defaultHeader = {
+  headerTintColor:"white",
+  headerStyle:{
+    backgroundColor:"red",
+    borderWidth: 5,
+    borderBottomColor: "black",
+    // color:"white", this doesn't work in header
+  
+  },
+  //  you have to use this (read the docs https://reactnavigation.org/docs/headers) for more options
+  headerTitleStyle:{
+    fontWeight:'bold',
+    fontStyle: "italic",
+    textDecorationLine:'line-through',
+}}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator 
+      initialRouteName="Home"
+      screenOptions={{
+        ...defaultHeader
+      }}
+      >
+
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen 
         name="Users" 
         options={{
           title:"xUsers", 
-          headerStyle:{
-            backgroundColor:"red",
-            borderWidth: 5,
-            borderBottomColor: "black",
-            // color:"white", this doesn't work in header
+          ...defaultHeader
           
-          },
-           headerTintColor:"white",
-          //  you have to use this (read the docs https://reactnavigation.org/docs/headers) for more options
-          headerTitleStyle:{
-            fontWeight:'bold',
-            fontStyle: "italic",
-            textDecorationLine:'line-through'
-          }
         }}
         component={Users} 
         initialParams={{id:1,codeName:"Pitboss"}}
